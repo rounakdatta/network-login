d5af7a95a78f9df6e19fbce3fd651ad4bcbf1223
rounakdatta12@gmail.com <rounakdatta12@gmail.com>
2017-12-25 00:38:47 +0530

    tuned

--- Python/gui.py
+++ Python/gui.py
@@ -8,6 +8,19 @@ from form import Ui_Dialog
 class MyDialog(QtGui.QDialog):
 	def __init__(self, parent=None):
 		QtGui.QWidget.__init__(self, parent)
+
+		image = QtGui.QLabel(self)
+		image.setGeometry(50, -100, 300, 300)
+		pixmap = QtGui.QPixmap("./logo.png")
+		image.setPixmap(pixmap)
+		image.show()
+
+		image2 = QtGui.QLabel(self)
+		image2.setGeometry(75, 125, 300, 300)
+		pixmap2 = QtGui.QPixmap("./cplogo.png")
+		image2.setPixmap(pixmap2)
+		image2.show()
+
 		self.ui = Ui_Dialog()
 		self.ui.setupUi(self)
 		self.ui.pushButton.clicked.connect(self.login)
@@ -16,7 +29,7 @@ class MyDialog(QtGui.QDialog):
 		ret = login(self.ui.lineEdit.text(), self.ui.lineEdit_2.text())
 		if(ret == "ok"):
 			boxy = QMessageBox()
-			boxy.setText("Connected")
+			boxy.setText("Connected to SRM Networks!")
 			boxy.exec_()
  
 if __name__ == "__main__":
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # network-login
-CheckPoint Network Login GUI
+Network Login interface
+
+![Screenshot](https://github.com/rounakdatta/network-login/tree/master/Python/screenshot.png)
